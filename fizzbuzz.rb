e87ab28f540ced9cac8ec7dc89d484c397641024
Gerardo Manzo <programmingmanzo@gmail.com>
2019-02-19 04:25:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@ def fizzbuzz(int)
     "Fizz"
     elsif int % 5 == 0
     "Buzz"
-    elsif int % 3 && 5 == 0
+    elsif (int % 3 == 0) && (int % 5 == 0)
     "FizzBuzz"
-  else int % 3 || 5 == 0
-    "FizzBuzz"
+  else (int % 3 == 0) || (int % 5 == 0)
+    "nil"
   end
 end
 
